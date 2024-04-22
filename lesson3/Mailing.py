88c4a6ffea1776b744e3dc86ff91a1537094c0b1
Kattssandra <itekaterina2023@yandex.by>
2024-04-22 12:38:40 +0300

    add second homework

--- lesson3/Mailing.py
+++ lesson3/Mailing.py
@@ -2,7 +2,7 @@ from Address import Address
 
 class Mailing:
      def __init__(self, to_address: Address, from_address: Address, cost: int, track: str):
-		     self.to_address = to_address
-		     self.from_address = from_address
-		     self.cost = cost
+             self.to_address = to_address
+             self.from_address = from_address
+             self.cost = cost
              self.track = track
--- lesson3/smartphone.py
+++ lesson3/smartphone.py
@@ -1,9 +1,9 @@
 class Smartphone:
     
     def __init__(self, marka, model, number):
-		     self.marka = marka
-		     self.model = model
-		     self.number = number
+             self.marka = marka
+             self.model = model
+             self.number = number
      
 	
     def sayphone(self):
