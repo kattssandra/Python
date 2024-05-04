209a534066987b9f6ea2f4d18702850391e6a10a
Kattssandra <itekaterina2023@yandex.by>
2024-05-04 19:23:17 +0300

    изменение 3 урок

--- lesson3/Mailing.py
+++ lesson3/Mailing.py
@@ -1,8 +1,8 @@
 from Address import Address
 
 class Mailing:
-     def __init__(self, to_address: Address, from_address: Address, cost: int, track: str):
-             self.to_address = to_address
-             self.from_address = from_address
+     def __init__(self, addressone: Address, addresstwo: Address, cost: int, track: str):
+             self.addressone = addressone
+             self.addresstwo = addresstwo
              self.cost = cost
              self.track = track
--- lesson3/lesson_3_task_3.py
+++ lesson3/lesson_3_task_3.py
@@ -1,10 +1,15 @@
 from Address import Address
 from Mailing import Mailing
 
-addressone = Address(index:"523", city: "Gomel", street: "Port", house: 17, flat: 2)
-addresstwo = Address(index:"475", city: "Minsk", street: "Mark", house: 77, flat: 5)
+addressone = Address(index ="523", city = "Gomel", street = "Port", house = 17, flat = 2)
+addresstwo = Address(index ="475", city = "Minsk", street = "Mark", house = 77, flat = 5)
 
 
 
-mail=Mailing(addressone, addresstwo, cost: 5412, track: "999")
-print(mail.track, mail.city, mail.street, mail.house, mail.flat)
+mail=Mailing(addressone, addresstwo, cost = 5412, track = "999")
+
+
+# Печать информации об отправлении
+print(f"Отправление {mail.track} из {mail.addressone.index}, {mail.addressone.city}, {mail.addressone.street}, {mail.addressone.house} - {mail.addressone.flat} "
+      f"в {mail.addresstwo.index}, {mail.addresstwo.city}, {mail.addresstwo.street}, {mail.addresstwo.house} - {mail.addresstwo.flat}. "
+      f"Стоимость {mail.cost} рублей")
